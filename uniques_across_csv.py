2e1f752c48ee5119fb9a24dc7766317d47459652
Surajit A Bose <verbose@gmail.com>
2023-07-18 01:25:59 -0700

    Update uniques_across_csv.py
    Handle file paths. Enable reciprocal file creation.

--- uniques_across_csv.py
+++ uniques_across_csv.py
@@ -10,10 +10,6 @@ Copyright © 2023 Surajit A. Bose
 import csv
 import os
 
-# Default output filename
-OUTFILE = 'uniques.csv'
-
-
 def getDataFromFile(first) :
     '''
     Get filename and file info from user.
@@ -39,6 +35,7 @@ def getDataFromFile(first) :
     while not valid_file :
         filename = input(f'Enter filename of the {prompt} csv: ')
         if os.path.isfile(filename) :
+                fpath, fname = os.path.split(filename)
                 valid_file = True
         else :
             print("File not found. Please check the path and filename, and include the .csv suffix.\n")
@@ -46,7 +43,7 @@ def getDataFromFile(first) :
     # Get column number
     valid_num = False
     while not valid_num : 
-        num = input(f'Enter the index of the column in {filename} with the data to check: ')
+        num = input(f'Enter the index of the column in {fname} with the data to check: ')
         try :
             col = int(num)
             valid_num = True
@@ -56,41 +53,40 @@ def getDataFromFile(first) :
     # Does file have header row?
     valid_head = False
     while not valid_head :
-        has_header = input(f'Does {filename} have a header row? [y/n]: ')
-        if has_header[0].lower() in ['y', 'n'] :
+        has_header = input(f'Does {fname} have a header row? [y/n]: ')[0].lower()
+        if has_header in ['y', 'n'] :
             valid_head = True
         else : 
             print('Invalid input, please try again.')
-    header = True if has_header == 'y' else False
     
     # Get data from file and add to set
     with open (filename, 'r') as csvfile:
         csv_rows = csv.reader(csvfile)
         
         # Skips header row
-        if header : 
+        if has_header == 'y' : 
             next(csv_rows)
         
         # Adds each elem to the set of known data
         for row in csv_rows:
             data_set.add(row[col]) 
     
-    return (filename, data_set)
+    return (filename, data_set)    
 
 
-def writeUniqueData(first_file, first_data, second_file, second_data) : 
+def writeUniqueData(xfile, xdata, yfile, ydata) : 
     '''
     Write out the data that is only in second file and not in first.
     
     Parameters
     ----------
-    first_file : string
+    xfile : string
         Filename of first file.
-    first_data : set
+    xdata : set
         Relevant data from first file.
-    second_file : string
+    yfile : string
         Filename of second file.
-    second_data : set
+    ydata : set
         Relevant data from second file.
 
     Raises
@@ -102,18 +98,70 @@ def writeUniqueData(first_file, first_data, second_file, second_data) :
     -------
     None.
     '''
-    uniq_data = second_data.difference(first_data) # new_data - orig_data
-    outfile = input(f'Enter filename to write unique data, or return for default {OUTFILE}: ')
-    if not outfile :
-        outfile = OUTFILE
+    uniq_data = ydata.difference(xdata) # new_data - orig_data
+    xpath, xname = os.path.split(xfile)
+    ypath, yname = os.path.split(yfile)
+    
+    print(f'\n*** NOTE: output file will be in same directory as {yname} ***\n')
+    
+    num = 1
+    default = 'unique' + str(num) + '.csv'
+    while os.path.isfile(os.path.join(ypath, default)) :
+        num += 1
+        default = 'unique' + str(num) + '.csv'
+    
+    valid_filename = False
+    while not valid_filename :
+        fname = input(f'Enter output filename, or return to use default {default}: ')
+        fout = fname if fname else default
+        outfile = os.path.join(ypath, fout) 
+        if os.path.isfile(outfile) :
+            print(f'{fout} already exists, please enter a unique filename.')
+        else :
+            valid_filename = True
+        
     try :
         with open (outfile, 'w', newline = '') as fh :
             writefile = csv.writer(fh)
-            writefile.writerow([f'Uniques in {second_file} not in {first_file}'])
+            writefile.writerow([f'Data in {yname} not in {xname}'])
             writefile.writerows([elem] for elem in uniq_data)        
-        print(f'Unique data successfully written to {outfile}.\n')
+        print(f'Unique data in {yname} successfully written to {fout}.\n')
     except IOError :
         raise SystemExit('Error writing output file')
+        
+
+def wantsViceVersa(first_file, first_data, second_file, second_data) :
+    '''
+    Check if user wants to write out unique data from first file too.
+    If yes, write out the data that is only in first file and not in second.
+
+    Parameters
+    ----------
+    first_file : string
+        Filename of first file.
+    first_data : set
+        Relevant data from first file.
+    second_file : string
+        Filename of second file.
+    second_data : set
+        Relevant data from second file.
+
+    Returns
+    -------
+    None.
+    '''
+    xpath, xname = os.path.split(first_file)
+    ypath, yname = os.path.split(second_file)
+    valid_want = False
+    while not valid_want :
+        wants_vice = input(f'Would you like a file with unique data in {first_file}? [y/n]: ')[0].lower()
+        if wants_vice in ['y', 'n'] :
+            valid_want = True
+        else : 
+            print('Invalid input, please try again.')
+    
+    if wants_vice == 'y' :
+        writeUniqueData(second_file, second_data, first_file, first_data)
 
 
 def getUniqueData() :
@@ -123,6 +171,7 @@ def getUniqueData() :
     first_file, first_data = getDataFromFile(True)
     second_file, second_data = getDataFromFile(False)
     writeUniqueData(first_file, first_data, second_file, second_data) 
+    wantsViceVersa(first_file, first_data, second_file, second_data)
 
 
 if __name__ == '__main__' :
